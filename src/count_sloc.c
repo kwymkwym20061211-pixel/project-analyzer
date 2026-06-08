283f427c3609dfb8267cc02dc2d20bf875c0dfd5
blue cushion <kwym.kwym20061211@gmail.com>
2026-06-09 02:22:13 +0900

    isspace

--- src/count_sloc.c
+++ src/count_sloc.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 // -------------------------------- C言語風 -------------------------------------------
 
 /**
@@ -12,6 +13,8 @@ int count_c_like_file(const unsigned char *text, size_t text_len) {
     bool is_in_block_comment = false;
     for (size_t i = 0; i < text_len; i++) {
         unsigned char c = text[i];
+        // 空白文字は無視する。
+        if (isspace(c)) continue;
         // まだ最後の文字でない場合は次の文字も見て、コメント開始/終了を判定する。
         if (i + 1 < text_len) {
             unsigned char next_c = text[i + 1];
@@ -38,8 +41,6 @@ int count_c_like_file(const unsigned char *text, size_t text_len) {
             is_empty_line = true;
             line_count++;
             continue;
-        } else if (c != ' ' && !has_comment_begun) {
-            is_empty_line = false;
         }
     }
     return line_count;
