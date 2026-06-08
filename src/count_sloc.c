ed2228bd75e43bbaf126a4d4d1ff8a63ba4c9cf9
blue cushion <kwym.kwym20061211@gmail.com>
2026-06-09 02:24:07 +0900

    format

--- src/count_sloc.c
+++ src/count_sloc.c
@@ -5,16 +5,16 @@
   * C言語風のファイルの行数をカウントする関数
  */
 int count_c_like_file(const unsigned char *text, size_t text_len) {
-    // コメント範囲を無視する+空でない行数を数えるの複合。
-    // コメント範囲のテキストを何もないものとみなして、行数を数える。
+    /*
+    * 行コメント（//）とブロックコメント（/* ... * /）を考慮して、空行やコメント行を除外して行数をカウントする。
+    * 行コメントは、そこより前に
+    */
     int line_count = 0;
     bool is_empty_line = true;
     bool has_comment_begun = false;
     bool is_in_block_comment = false;
     for (size_t i = 0; i < text_len; i++) {
         unsigned char c = text[i];
-        // 空白文字は無視する。
-        if (isspace(c)) continue;
         // まだ最後の文字でない場合は次の文字も見て、コメント開始/終了を判定する。
         if (i + 1 < text_len) {
             unsigned char next_c = text[i + 1];
@@ -40,6 +40,11 @@ int count_c_like_file(const unsigned char *text, size_t text_len) {
             has_comment_begun = false;
             is_empty_line = true;
             line_count++;
+            continue;
+        }
+        // コメント内にいない時で、空白系以外を発見したら空行でないとみなす。
+        if (!isspace(c) && !is_in_block_comment && !has_comment_begun) {
+            is_empty_line = false;
             continue;
         }
     }
